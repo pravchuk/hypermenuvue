485e018d0118783923f747a344fcd294a95d6456
pravchuk-fw <praveen.chukka@freshworks.com>
2020-05-29 13:56:54 +0530

    Cookview

---
+++ app/cook-view/src/App.vue
@@ -0,0 +1,90 @@
+<template>
+  <div id="app">
+    <!-- <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <div class="top">
+      HyperMenu
+    </div>
+    <div class="pagename">
+      Orders
+    </div>
+    <div class="orders">
+      <Order :key="order.id" :order="order" v-for="order in orders" />
+    </div>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import Order from './components/Order.vue'
+
+export default {
+  name: 'App',
+  data: function(){
+    return {
+    orders:[
+    {
+      id: '123',
+      table: '12',
+      items: [
+        {
+          name: 'Gobi Manchuri'
+        },
+        {
+          name: 'Jeera Rice',
+          quantity: 2
+        },
+        {
+          name: 'Chilly Chicken',
+          specialInstruction: 'less spicy'
+        }
+      ]
+    },
+    {
+      id: '124',
+      table: '13',
+      items: [
+        {
+          name: 'Gobi Manchuri'
+        },
+        {
+          name: 'Jeera Rice',
+          quantity: 2
+        },
+        {
+          name: 'Chilly Chicken',
+          specialInstruction: 'less spicy'
+        }
+      ]
+    }
+  ]
+  }},
+  components: {
+    Order
+    // HelloWorld
+  }
+}
+</script>
+
+<style scoped>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  /* text-align: center; */
+  color: #2c3e50;
+  /* margin-top: 60px; */
+}
+.top{
+  text-align:center;
+  background:coral;
+  color: white;
+}
+.pagename{
+  background: red;
+  color: white;
+}
+.orders{
+  margin-top: 20px;
+}
+</style>
---
+++ app/cook-view/src/components/Order.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="order" @click="openOrderActions = !openOrderActions">
+        <span class="order-id">#{{order.id}}</span>
+        <span class="table">Table : {{order.table}}</span>
+        <ul class="items">
+            <li :key="item.name" v-for="item in order.items">{{item.name}} <span v-if="item.quantity">( {{item.quantity}}x )</span> <span v-if="item.specialInstruction">( {{item.specialInstruction}} )</span></li>
+        </ul>
+        <span class="num-items">{{numberOfItems}} items</span>
+        <div class="orderActions">
+            <button class="b-order">Order</button>
+            <button class="b-reject">Reject</button>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'Order',
+    props:{
+        order: Object
+    },
+    computed: {
+        numberOfItems: function(){
+            let count = 0;
+            for(let item of this.order  .items){
+                if(item.quantity){
+                    count += item.quantity;
+                }else{
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+@import 'src/assets/css/buttons.scss';
+
+.order{
+    padding: 20px;
+    margin: 20px;
+    background: rgb(255, 156, 156);
+}
+.order-id{
+    position:absolute;
+    right: 50px;
+}
+.num-items{
+    font-style: italic;
+}
+</style>
